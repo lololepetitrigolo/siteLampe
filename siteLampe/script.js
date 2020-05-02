20edba93d3ab7ba2dba751d1cedb39e0b3c04503
lololepetitrigolo <62268724+lololepetitrigolo@users.noreply.github.com>
2020-05-02 17:35:54 +0200

    Add files via upload

---
+++ siteLampe/script.js
@@ -0,0 +1,46 @@
+  window.addEventListener("load", function () {
+  function sendData() {
+    var XHR = new XMLHttpRequest();
+
+    // Liez l'objet FormData et l'élément form
+    var FD = new FormData(form);
+
+    var api_key = document.getElementById('api_key').value;
+    var field1 = document.getElementById('field1').value;
+    var field2 = document.getElementById('field2').value;
+    var field3 = document.getElementById('field3').value;
+    var field4 = document.getElementById('field4').value;
+    var field5 = document.getElementById('field5').value;
+
+    var url = "https://api.thingspeak.com/update?api_key="+ api_key +"&field1="+field1+"&field2="+field2+"&field3="+field3+"&field4="+field4+"&field5="+field5;
+
+    // Définissez ce qui se passe si la soumission s'est opérée avec succès
+    XHR.addEventListener("load", function(event) {
+      console.log(event.target.responseText);
+      if (event.target.responseText==0) {
+        document.getElementById('body').style.background = "red";
+      }
+      else{
+        document.getElementById('body').style.background = "#24292E";
+      }
+    });
+
+    console.log(url);
+
+    // Configurez la requête
+    XHR.open("GET", url);
+
+    // Les données envoyées sont ce que l'utilisateur a mis dans le formulaire
+    XHR.send(FD);
+  }
+
+  // Accédez à l'élément form …
+  var form = document.getElementById("myForm");
+
+  // … et prenez en charge l'événement submit.
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    sendData();
+  });
+});
